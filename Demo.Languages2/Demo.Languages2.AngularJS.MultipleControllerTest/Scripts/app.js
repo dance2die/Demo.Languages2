bf9cee3e153395b15448e4e66a013f38eb76d03a
dance2die <dontbothermewithspam@gmail.com>
2015-12-22 15:57:52 -0500

    Yes, it's possible to have 2 different controllers with one module

---
+++ Demo.Languages2/Demo.Languages2.AngularJS.MultipleControllerTest/Scripts/app.js
@@ -0,0 +1,15 @@
+﻿(function() {
+
+	angular.module("app", [])
+		.controller("mainCtrl", ['$scope', '$http', function ($scope, $http) {
+			$scope.button1Click = function() {
+				alert("button1 clicked");
+			}
+		}])
+		.controller("secondCtrl", ['$scope', '$http', function ($scope, $http) {
+			$scope.button2Click = function () {
+				alert("button2 clicked");
+			}
+		}]);
+
+})();
