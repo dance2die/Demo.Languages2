4e743941ebb22a807024ab699598040100962d37
dance2die <dontbothermewithspam@gmail.com>
2015-12-22 20:54:47 -0500

    directive not working...

---
+++ Demo.Languages2/Demo.Languages2.AngularJS.MultipleControllerTest/Scripts/directives/area1.js
@@ -0,0 +1,14 @@
+﻿(function() {
+	angular.module("app")
+		.directive("areaOne", function() {
+			return {
+				restrict: 'E',
+				templateUrl: '~/templates/area1.html',
+				controller: function ($scope) {
+					$scope.button1Click = function () {
+						alert("button1 clicked");
+					}
+				}
+			}
+		});
+})();
---
+++ Demo.Languages2/Demo.Languages2.AngularJS.MultipleControllerTest/Scripts/directives/area2.js
@@ -0,0 +1,15 @@
+﻿(function() {
+	angular.module("app")
+		.directive("areaTwo", function () {
+			return {
+				restrict: 'E',
+				replace: true,
+				templateUrl: '../templates/area2.html',
+				controller: function ($scope) {
+					$scope.button2Click = function () {
+						alert("button2 clicked");
+					}
+				}
+			}
+		});
+})();
