b4874af0df14db0119baa71d2b85308eb6b57e2b
dance2die <dontbothermewithspam@gmail.com>
2015-12-25 14:47:59 -0500

    Shape module

--- Demo.Languages2/Demo.Languages2.TypeScriptTest01/app.ts
+++ Demo.Languages2/Demo.Languages2.TypeScriptTest01/app.ts
@@ -3,17 +3,7 @@
 /// <reference path="scripts/typings/angularjs/angular-resource.d.ts" />
 /// <reference path="scripts/typings/toastr/toastr.d.ts" />
 
-
-
-module MainModule {
-	$(document).ready(() => {
-		//toastr.info("This is a toastr");
-
-		var rect: IRectangle = new Rectangle(10, 4);
-		var area: number = rect.getArea();
-		toastr.info("area = " + area.toString());
-	});
-
+module Shape {
 	export interface IRectangle {
 		width: number;
 		height: number;
@@ -28,5 +18,21 @@ module MainModule {
 			return this.width * this.height;
 		}
 	}
+}
 
+module Program {
+	import Rectangle = Shape.Rectangle;
+	import IRectangle = Shape.IRectangle;
+
+	function main() {
+		//toastr.info("This is a toastr");
+
+		var rect: IRectangle = new Rectangle(10, 4);
+		var area: number = rect.getArea();
+		toastr.info("area = " + area.toString());
+	}
+
+	$(() =>{
+		main();
+	});
 }
