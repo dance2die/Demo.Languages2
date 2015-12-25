ecaa2b2417fa805673ae2cea756f1ab73d4ad3d1
dance2die <dontbothermewithspam@gmail.com>
2015-12-25 13:46:32 -0500

    added TypeScriptTest01

---
+++ Demo.Languages2/Demo.Languages2.TypeScriptTest01/app.ts
@@ -0,0 +1,9 @@
+﻿/// <reference path="scripts/typings/jquery/jquery.d.ts" />
+/// <reference path="scripts/typings/angularjs/angular.d.ts" />
+/// <reference path="scripts/typings/angularjs/angular-resource.d.ts" />
+
+(() => {
+	
+
+
+})();
