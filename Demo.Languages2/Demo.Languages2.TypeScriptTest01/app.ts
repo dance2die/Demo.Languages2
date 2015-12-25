6470222466092112b0ca9d50300d85962c65fe43
dance2die <dontbothermewithspam@gmail.com>
2015-12-25 14:39:26 -0500

    learned about Module in typescript

--- Demo.Languages2/Demo.Languages2.TypeScriptTest01/app.ts
+++ Demo.Languages2/Demo.Languages2.TypeScriptTest01/app.ts
@@ -1,9 +1,32 @@
 ﻿/// <reference path="scripts/typings/jquery/jquery.d.ts" />
 /// <reference path="scripts/typings/angularjs/angular.d.ts" />
 /// <reference path="scripts/typings/angularjs/angular-resource.d.ts" />
-
-(() => {
-	
+/// <reference path="scripts/typings/toastr/toastr.d.ts" />
 
 
-})();
+
+module MainModule {
+	$(document).ready(() => {
+		//toastr.info("This is a toastr");
+
+		var rect: IRectangle = new Rectangle(10, 4);
+		var area: number = rect.getArea();
+		toastr.info("area = " + area.toString());
+	});
+
+	export interface IRectangle {
+		width: number;
+		height: number;
+		getArea(): number;
+	}
+
+	export class Rectangle implements IRectangle {
+		constructor(public width: number, public height: number) {
+		}
+
+		getArea(): number {
+			return this.width * this.height;
+		}
+	}
+
+}
