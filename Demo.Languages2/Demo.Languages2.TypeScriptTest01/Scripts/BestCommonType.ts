a6a07e55c2ce685e0224fa9fe06441b897123153
dance2die <dontbothermewithspam@gmail.com>
2015-12-26 12:56:00 -0500

    Learned about Typescript's best common type behavior

---
+++ Demo.Languages2/Demo.Languages2.TypeScriptTest01/Scripts/BestCommonType.ts
@@ -0,0 +1,28 @@
+﻿/// <reference path="typings/toastr/toastr.d.ts" />
+
+
+module BestCommonExample {
+
+	var bestCommonTypeExample = [
+		{ a: 'A', b: 1, c: true },
+		{ a: 'B', b: 2 },
+		{ b: 3 }
+	];
+
+	export class BestCommonExampleClass {
+		run() {
+			for (var i = 0; i < bestCommonTypeExample.length; i++) {
+				var value = bestCommonTypeExample[i];
+
+				// The property 'a' does not exist on value of type '{ b: number }'.
+				//console.log(value.a);
+
+				// OK
+				console.log(value.b);
+
+				// The property 'c' does not exist on value of type '{ b: number }'.
+				//console.log(value.c);
+			}
+		}
+	}
+}
--- Demo.Languages2/Demo.Languages2.TypeScriptTest01/app.ts
+++ Demo.Languages2/Demo.Languages2.TypeScriptTest01/app.ts
@@ -2,6 +2,7 @@
 /// <reference path="scripts/typings/angularjs/angular.d.ts" />
 /// <reference path="scripts/typings/angularjs/angular-resource.d.ts" />
 /// <reference path="scripts/typings/toastr/toastr.d.ts" />
+/// <reference path="scripts/bestcommontype.ts" />
 
 module Shape {
 	export interface IRectangle {
@@ -23,13 +24,17 @@ module Shape {
 module Program {
 	import Rectangle = Shape.Rectangle;
 	import IRectangle = Shape.IRectangle;
+	import BestCommonExampleClass = BestCommonExample.BestCommonExampleClass;
 
 	function main() {
-		//toastr.info("This is a toastr");
+		////toastr.info("This is a toastr");
 
-		var rect: IRectangle = new Rectangle(10, 4);
-		var area: number = rect.getArea();
-		toastr.info("area = " + area.toString());
+		//var rect: IRectangle = new Rectangle(10, 4);
+		//var area: number = rect.getArea();
+		//toastr.info("area = " + area.toString());
+
+		var example1: BestCommonExampleClass = new BestCommonExampleClass();
+		example1.run();
 	}
 
 	$(() =>{
