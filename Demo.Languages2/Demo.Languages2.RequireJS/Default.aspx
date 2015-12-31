1cd5ed8072768b293799bc9f8d0e4aef88f16e97
dance2die <dontbothermewithspam@gmail.com>
2015-12-31 13:10:16 -0500

    Simple jquery loading using RequireJS

--- Demo.Languages2/Demo.Languages2.RequireJS/Default.aspx
+++ Demo.Languages2/Demo.Languages2.RequireJS/Default.aspx
@@ -5,6 +5,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
 	<title></title>
+
+	<script src="Scripts/require.js" data-main="Scripts/main.js"></script>
 </head>
 <body>
 	<form id="form1" runat="server">
---
+++ Demo.Languages2/Demo.Languages2.RequireJS/Scripts/main.js
@@ -0,0 +1,25 @@
+﻿require.config({
+	paths: {
+		jquery: "jquery-2.1.4.min"
+	}
+});
+
+
+define("task1", [], function() {
+	"use strict";
+
+	function alert1(msg) {
+		alert(msg);
+	}
+
+	return{
+		alert1: alert1
+	}
+});
+
+
+require(["jquery", "task1"], function($, task1) {
+	task1.alert1("Hello RequireJS World!!!");
+});
+
+
