8476b0cef493cb4d0cf6d7c17495eeb4e35c59dd
dance2die <dontbothermewithspam@gmail.com>
2015-12-31 13:19:48 -0500

    Learned how to use CommonJS syntax with RequireJS

--- Demo.Languages2/Demo.Languages2.RequireJS/Scripts/main.js
+++ Demo.Languages2/Demo.Languages2.RequireJS/Scripts/main.js
@@ -6,21 +6,33 @@
 });
 
 
-define("task1", ["toastr"], function (toastr) {
-	"use strict";
+// "task1" using AMD (Asynchronous Module Declaration) syntax
+//define("task1", ["toastr"], function (toastr) {
+//	"use strict";
 
-	function alert1(msg) {
-		//alert(msg);
+//	function alert1(msg) {
+//		//alert(msg);
+//		toastr.info(msg);
+//	}
+
+//	return{
+//		alert1: alert1
+//	}
+//});
+
+
+// "task1" defined using CommonJS syntax
+define("task1", function(require, exports, module) {
+	var $ = require("jquery");
+	var toastr = require('toastr');
+
+	exports.alert1 = function(msg) {
 		toastr.info(msg);
-	}
-
-	return{
-		alert1: alert1
 	}
 });
 
 
-require(["jquery", "task1"], function($, task1) {
+require(["task1"], function(task1) {
 	task1.alert1("Hello RequireJS World!!!");
 });
 
