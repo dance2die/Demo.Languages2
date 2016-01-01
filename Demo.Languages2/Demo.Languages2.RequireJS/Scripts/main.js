a0cd4b1ad4a967849ddcacec7e57fc268bff61ef
dance2die <dontbothermewithspam@gmail.com>
2016-01-01 11:00:47 -0500

    loading remote module

--- Demo.Languages2/Demo.Languages2.RequireJS/Scripts/main.js
+++ Demo.Languages2/Demo.Languages2.RequireJS/Scripts/main.js
@@ -2,7 +2,8 @@
 	baseUrl: "Scripts/",
 	paths: {
 		jquery: "jquery-2.1.4.min",
-		toastr: "toastr.min"
+		toastr: "toastr.min",
+		task1: "tasks/task1"
 	},
 	shim : {
 		underscore : {
@@ -34,19 +35,6 @@
 //		alert1: alert1
 //	}
 //});
-
-
-// "task1" defined using CommonJS syntax
-define("task1", function(require, exports, module) {
-	//var $ = require("jquery");
-	var toastr = require('toastr');
-
-	exports.alert1 = function (msg) {
-		$.each([1, 2, 3], function(index, value) {
-			toastr.info(msg + " " + value.toString());
-		});
-	}
-});
 
 
 require(["task1", "config"], function (task1, config) {
---
+++ Demo.Languages2/Demo.Languages2.RequireJS/Scripts/tasks/task1.js
@@ -0,0 +1,11 @@
+﻿// "task1" defined using CommonJS syntax
+define("task1", function (require, exports, module) {
+	//var $ = require("jquery");
+	var toastr = require('toastr');
+
+	exports.alert1 = function (msg) {
+		$.each([1, 2, 3], function (index, value) {
+			toastr.info(msg + " " + value.toString());
+		});
+	}
+});
