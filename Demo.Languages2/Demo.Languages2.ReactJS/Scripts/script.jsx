8fa5a01804103de853adff9c997d3152b8558ae7
dance2die <dontbothermewithspam@gmail.com>
2016-01-10 10:11:17 -0500

    First ReactJS Component test success.

---
+++ Demo.Languages2/Demo.Languages2.ReactJS/Scripts/script.jsx
@@ -0,0 +1,12 @@
+﻿var Button = React.createClass({
+	getInitialState: function() {
+		return { counter: 0 };
+	},
+	render: function() {
+		return (
+			<button>{this.state.counter}</button>
+		);
+	}
+});
+
+React.render(<Button />, document.getElementById("root"));
