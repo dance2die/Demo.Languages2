d3711b4bfccb2ff249271dc71c8d80fa954f1216
dance2die <dontbothermewithspam@gmail.com>
2016-01-14 19:57:05 -0500

    Alertify test.

--- Demo.Languages2/Demo.Languages2.MixingAngularTypeScriptRequireJS/Default.aspx
+++ Demo.Languages2/Demo.Languages2.MixingAngularTypeScriptRequireJS/Default.aspx
@@ -5,19 +5,23 @@
 <html xmlns="http://www.w3.org/1999/xhtml" ng-app="app">
 <head runat="server">
 	<title></title>
-	
+
 	<link href="Content/bootstrap.min.css" rel="stylesheet" />
 	<link href="Content/toastr.css" rel="stylesheet" />
+	<link href="Content/alertify/alertify.core.css" rel="stylesheet" />
+	<link href="Content/alertify/alertify.default.css" rel="stylesheet" />
+	<link href="Content/alertify/alertify.bootstrap.css" rel="stylesheet" />
 </head>
 <body ng-controller="mainCtrl">
+
 	<form id="form1" runat="server">
 		<div>
 		</div>
 	</form>
-	
+
 	<textarea id="result" rows="4" cols="50" ng-model="result"></textarea>
-	
-	
+
+
 	<script src="Scripts/require.js" data-main="js/main.js"></script>
 </body>
 </html>
--- Demo.Languages2/Demo.Languages2.MixingAngularTypeScriptRequireJS/js/main.js
+++ Demo.Languages2/Demo.Languages2.MixingAngularTypeScriptRequireJS/js/main.js
@@ -2,7 +2,8 @@ require.config({
 	paths: {
 		jquery: "../Scripts/jquery-2.1.4.min",
 		toastr: "../Scripts/toastr.min",
-		angular: "../Scripts/angular.min"
+		angular: "../Scripts/angular.min",
+		alertify: "../Scripts/alertify/alertify.min"
 	},
 	shim: {
 		jquery: {
@@ -18,9 +19,13 @@ require.config({
 //import app = require("app");
 
 
-require(["toastr", "app"], function(toastr, app) {
-	toastr.info("Hello world");
+require(["toastr", "app", "alertify"], function (toastr, app, alertify) {
+	//toastr.info("Hello world");
 
-	var angularBootstrap = new app.AngularBootstrap();
-	angularBootstrap.init();
+	//var angularBootstrap = new app.AngularBootstrap();
+	//angularBootstrap.init();
+
+	alertify.log("alertify.log");
+	alertify.success("Saved successfully.");
+	alertify.error("Error notification");
 });
