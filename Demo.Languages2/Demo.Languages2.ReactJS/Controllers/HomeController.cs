5a4dda6121a88d2014e4432f8263a84290266da8
dance2die <dontbothermewithspam@gmail.com>
2016-01-17 13:48:05 -0500

    bootstrapping code for /home/game

--- Demo.Languages2/Demo.Languages2.ReactJS/Controllers/HomeController.cs
+++ Demo.Languages2/Demo.Languages2.ReactJS/Controllers/HomeController.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Demo.Languages2.ReactJS.Controllers
 {
 	public class HomeController : Controller
 	{
-		// GET: Home
 		public ActionResult Index()
 		{
 			return View();
 		}
 
 		public ActionResult Card()
+		{
+			return View();
+		}
+
+		public ActionResult Game()
 		{
 			return View();
 		}
