45c859697a3b114ed9ac222a1ceb0c706140ac6d
dance2die <dontbothermewithspam@gmail.com>
2016-01-31 16:12:08 -0500

    can inject jquery but not alertify, yet.

---
+++ Demo.Languages2/Demo.Languages2.ReactJSEssentials/Controllers/Chapter03Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.Languages2.ReactJSEssentials.Controllers
+{
+    public class Chapter03Controller : Controller
+    {
+        // GET: Chapter03
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Demo.Languages2/Demo.Languages2.ReactJSEssentials/Scripts/chapter03-main.js
@@ -0,0 +1,20 @@
+﻿require.config({
+	baseUrl: "Scripts/",
+	paths: {
+		jquery: "jquery-2.2.0.min",
+		alertify: "alertify/alertify.min"
+	},
+	shim: {
+		jquery: {
+			exports: "$"
+		}
+	}
+});
+
+require([], function () {
+	//alertify.info("Hello Chapter03-main.js!");
+	$.each([1, 2, 3], function(index, value) {
+		console.log("index = " + index + "; value = " + value);
+	});
+});
+
