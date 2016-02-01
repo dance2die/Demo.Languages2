5b34bac7255448bdf41c95f9119a44a3f8277de2
dance2die <dontbothermewithspam@gmail.com>
2016-02-01 12:18:41 -0500

    Set date format using Momentjs

--- Demo.Languages2/Demo.Languages2.ReactJS/Scripts/jsx/hello-react.jsx
+++ Demo.Languages2/Demo.Languages2.ReactJS/Scripts/jsx/hello-react.jsx
@@ -1,11 +1,20 @@
 ﻿// https://facebook.github.io/react/docs/displaying-data.html
 
+function addDays(date, days) {
+	var result = new Date(date);
+	result.setDate(result.getDate() + days);
+	return result;
+}
+
 var HelloWorld = React.createClass({
 	render: function () {
+		var mmddyyyy = moment(addDays(this.props.date, 10)).format("MM/DD/YYYY hh:mm:ss a");
+		// It is {this.props.date.toTimeString()}
 		return (
 		  <p>
 		  	Hello, <input type="text" placeholder="Your name here" />!
-		  	It is {this.props.date.toTimeString()}
+		  	
+		  	It is {mmddyyyy}
 		  </p>
 	  );
 	}
